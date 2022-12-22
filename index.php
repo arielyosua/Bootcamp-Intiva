f40deacf8c0528f5c2da7c601d7d8c8cde5e530d
arielyosua <120299384+arielyosua@users.noreply.github.com>
2022-12-22 18:39:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$a=100;
+
+if ($a<45){
+       echo "Indeks Nilai : D </br>";
+       echo "Keterangan : Tidak Lulus";
+}else if ($a==45){
+       echo "Indeks Nilai : C </br>";
+       echo "Keterangan : Cukup";
+}else if ($a<=70){
+       echo "Indeks Nilai : B </br>";
+       echo "Keterangan : Baik";
+}else if ($a>70){
+       echo "Indeks Nilai : A </br>";
+       echo "Keterangan : Mantab";
+
+}
+?>
